bc84d2885e6a722b22d61178d133ae51bcb3a037
movmario <mario.mov@protonmail.com>
2022-07-02 19:01:57 -0400

    Casting de valores

--- CalculadoraDeEstoque/src/CalculadoraDeEstoque.java
+++ CalculadoraDeEstoque/src/CalculadoraDeEstoque.java
@@ -2,12 +2,8 @@ public class CalculadoraDeEstoque {
     
     public static void main(String[] args) {
         
-        double LivroJava8 = 59.90;
-        double LivroTDD = 59.90;
-
-        double soma = LivroJava8 + LivroTDD;
-
-        System.out.println("O Total em estoque é " + soma);
+        double LivroJava8 = 60;
+        int numeroInteiro = (int) LivroJava8;
         
 
     }
