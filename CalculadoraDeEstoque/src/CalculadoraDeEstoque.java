db155f7502736fb982b0d3f8035ade50899a8eae
movmario <mario.mov@protonmail.com>
2022-07-02 18:55:10 -0400

    Declaração e atribuição de variáveis

---
+++ CalculadoraDeEstoque/src/CalculadoraDeEstoque.java
@@ -0,0 +1,17 @@
+public class CalculadoraDeEstoque {
+    
+    public static void main(String[] args) {
+        
+        double LivroJava8 = 59.90;
+        double LivroTDD = 59.90;
+
+        double soma = LivroJava8 + LivroTDD;
+
+        System.out.println("O Total em estoque é " + soma);
+        
+
+    }
+
+
+
+}
