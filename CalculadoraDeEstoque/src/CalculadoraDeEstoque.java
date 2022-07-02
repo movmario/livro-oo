6f18cb07e75e3a0028bfbd85d57dbeb7958b340f
movmario <mario.mov@protonmail.com>
2022-07-02 19:18:46 -0400

    Adicionando condicionais

--- CalculadoraDeEstoque/src/CalculadoraDeEstoque.java
+++ CalculadoraDeEstoque/src/CalculadoraDeEstoque.java
@@ -2,9 +2,22 @@ public class CalculadoraDeEstoque {
     
     public static void main(String[] args) {
         
-        double LivroJava8 = 60;
-        int numeroInteiro = (int) LivroJava8;
-        
+        double livroJava8 = 59.90;
+        double livroTDD = 59.90;
+
+        double soma = livroJava8 + livroTDD;
+
+        System.out.println("O Total em estoque é " + soma);
+
+        if (soma < 150) {
+            System.out.println("Seu estoque está muito baixo!");
+            } else if (soma >= 2000) {
+                System.out.println("Seu estoque está muito alto");
+            } else {
+                System.out.println("Seu estoque está bom");
+            } 
+
+
 
     }
 
