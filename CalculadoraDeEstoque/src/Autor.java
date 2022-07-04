91eacccaab65d07630085badc7988a149966c5cd
movmario <mario.mov@protonmail.com>
2022-07-04 17:42:37 -0400

    Criando molde autor

---
+++ CalculadoraDeEstoque/src/Autor.java
@@ -0,0 +1,15 @@
+
+public class Autor {
+	String email;
+	String cpf;
+	String nome;
+	
+	void mostrarDetalhes() {
+		System.out.println("Mostrando detalhes dp autor");
+		System.out.println("Nome: " + nome);
+		System.out.println("Email: " + email);
+		System.out.println("CPF: " + cpf);
+		
+	}
+	
+}
